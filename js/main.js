c37aceca3b3ee67b8a3a07ad2326218536a8c2a1
Christopher Glinka <glinkachris@gmail.com>
2021-10-12 11:28:32 -0600

    Comment
    Need to ask instructor about a reference variable.

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ window.addEventListener('load', function(){
     const bottomText = document.querySelector(".memeBottomText")
     const generateButton = document.querySelector('.submit')
     const resetButton = document.querySelector('.reset')
-    const warning = document.querySelector(".warning");
     const memeDisplay = document.querySelector('.meme-display')
     const topTextDisplay = document.querySelector('.top-text')
     const bottomTextDisplay = document.querySelector('.bottom-text')
+    // ASK ABOUT THE BELOW VARIABLE!!!
+    const warning = document.querySelector(".warning");
 
     console.log(memeGenerator, imageSelect, topText, bottomText, generateButton, resetButton, topTextDisplay, bottomTextDisplay, memeDisplay)
 })
