487169cfb426b89a7d931e72a84187ed4156b657
Christopher Glinka <glinkachris@gmail.com>
2021-10-12 21:18:34 -0600

    Validation and user input
    Code for user input capture and validation.

--- js/main.js
+++ js/main.js
@@ -9,8 +9,60 @@ window.addEventListener('load', function(){
     const memeDisplay = document.querySelector('.meme-display')
     const topTextDisplay = document.querySelector('.top-text')
     const bottomTextDisplay = document.querySelector('.bottom-text')
-    // ASK ABOUT THE BELOW VARIABLE!!!
+    // ASK ABOUT THE BELOW REFERENCE VARIABLE!!!
     const warning = document.querySelector(".warning");
+    
+    memeGenerator.addEventListener("submit", function(e){
+        e.preventDefault()
 
-    console.log(memeGenerator, imageSelect, topText, bottomText, generateButton, resetButton, topTextDisplay, bottomTextDisplay, memeDisplay)
+        // Variable declarations
+        let memeSelect = Option(imageSelect.value.trim());
+        let topTextInput = textContent(topText.value.trim());
+        let bottomTextInput = textContent(bottomText.value.trim());
+
+        // **********Image validation***********
+        if(isNaN(memeSelect)) {
+            warning.classList.remove('hide')
+            warning.querySelector('span').textContent = "A selection is required"
+        }
+        else {
+            warning.classList.add('hide');
+            memeDisplay.value = memeSelect
+        }
+
+        // **********topText validation***********
+        if(isNaN(topTextInput)) {
+            warning.classList.remove('hide')
+            warning.querySelector('span').textContent = "An input is required"
+        }
+        else {
+            warning.classList.add('hide');
+            topTextDisplay.value = topTextInput
+        }
+
+        // **********bottomText validation***********
+        if(isNaN(bottomTextInput)) {
+            warning.classList.remove('hide')
+            warning.querySelector('span').textContent = "An input is required"
+        }
+        else {
+            warning.classList.add('hide');
+            bottomTextDisplay.value = bottomTextInput
+        }
+
+        // Capture user input(topText)?
+        topText.addEventListener('input', function(e){
+            warning.classList.add('hide')
+        })
+        // Focus cursor?
+        topText.addEventListener('focus', function(e){
+            warning.classList.add('hide')
+            e.target.value=""
+        })
+
+        // Capture user input(bottomText)?
+        bottomText.addEventListener('input', function(e){
+            warning.classList.add('hide')
+        })
+    })
 })
